23299a7f777288f6c2e3fcb83798059c97fc1c3b
Maxim <maxkadet80@gmail.com>
2024-12-03 22:14:09 +0300

    Delete text.js

--- text.js
+++
@@ -1,6 +0,0 @@
-async function loadAboutText() {
-    const response = await fetch('src/about.txt');
-    const text = await response.text();
-    document.getElementById('about-text').innerHTML = text.replace(/\n/g, '<br>');
-  }
-  window.onload = loadAboutText;
